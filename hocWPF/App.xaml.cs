e77620fb91470003f1c64c1ef82a428f494ade71
LuNguyenDuyDuc <duclnd.t1.2023@gmail.com>
2024-11-12 15:46:19 +0700

    Add a project

---
+++ hocWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace hocWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
